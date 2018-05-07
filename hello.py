cc9306b132991565aa0fbd7ae3dc17fa2bbf8534
Dawlatly <7oda95@gmail.com>
2018-05-07 16:42:46 +0800

    fix: data.json instead of data.txt

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ from run import parseAddress
 form = cgi.FieldStorage() 
 address = parseAddress(str(form.getvalue('address')))
 
-with open('data.txt', 'w+') as outfile:  
+with open('data.json', 'w+') as outfile:  
     json.dump(address, outfile)
 
 print ("Content-type:text/html\r\n\r\n")
